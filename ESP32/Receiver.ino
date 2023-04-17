8bff6b6046b6627452027fb7d6e2160f8e84ae62
munteam30 <107911212+munteam30@users.noreply.github.com>
2023-04-17 13:46:50 +0300

    Create Receiver.ino
    Codul pentru receive. Nu este final sau testat cum ar functiona cu transmiterea datelor procesate

---
+++ ESP32/Receiver.ino
@@ -0,0 +1,27 @@
+//Receiver ---> COM7
+#include <esp_now.h>
+#include <WiFi.h>
+
+uint8_t receivedData[30];
+int data_len = 30;
+
+void onDataReceived(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+char buff[data_len + 1];
+memcpy(buff, data, data_len);
+buff[data_len] = '\0';
+String buffStr = String(buff);
+Serial.println(buffStr);
+}
+
+void setup() {
+Serial.begin(115200);
+WiFi.mode(WIFI_STA);
+if (esp_now_init() != ESP_OK) {
+Serial.println("Error initializing ESP-NOW");
+return;
+}
+esp_now_register_recv_cb(onDataReceived);
+}
+
+void loop() {
+}
